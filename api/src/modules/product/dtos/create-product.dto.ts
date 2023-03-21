2c2d89185288d00339f76f0b30b83c5bb9314668
Quyen Nguyen <nguyenhuuquyen2001@gmail.com>
2023-03-21 22:35:40 +0700

    config class-validator

--- api/src/modules/product/dtos/create-product.dto.ts
+++ api/src/modules/product/dtos/create-product.dto.ts
@@ -1,9 +1,23 @@
+import { IsNotEmpty, IsNumber, IsString } from "class-validator"
 import { Category } from "../schemas/product.schema"
 
 export class CreateProductDto {
-  readonly name: string
-  readonly description: string
-  readonly brand: string
-  readonly price: number
+  @IsNotEmpty()
+  @IsString()
+  name: string
+
+  @IsNotEmpty()
+  @IsString()
+  description: string
+
+  @IsNotEmpty()
+  @IsString()
+  brand: string
+
+  @IsNotEmpty()
+  @IsNumber()
+  price: number
+
+  @IsNotEmpty()
   readonly category: Category
 }
