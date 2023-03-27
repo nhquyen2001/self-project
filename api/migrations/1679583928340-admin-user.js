10d5c964a9f47a0043a4af10bd7b9e008da9c7f9
Quyen Nguyen <nguyenhuuquyen2001@gmail.com>
2023-03-27 21:54:38 +0700

    chore

--- api/migrations/1679583928340-admin-user.js
+++ api/migrations/1679583928340-admin-user.js
@@ -1,9 +1,39 @@
-'use strict'
+const {
+  DB, COLLECTION, hashPassword
+} = require('./lib');
 
-module.exports.up = function (next) {
-  next()
+const defaultPassword = 'adminadmin';
+
+module.exports.up = async function up(next) {
+  const user = {
+    firstName: 'Admin',
+    lastName: 'Admin',
+    email: `admin@${process.env.DOMAIN || 'example.com'}`,
+    username: 'admin',
+    roles: ['admin', 'user'],
+    status: 'active',
+    emailVerified: true,
+    verifiedEmail: true,
+    password: hashPassword(defaultPassword, 10)
+  };
+
+  try {
+    // eslint-disable-next-line no-console
+    console.log(`Seeding ${user.username}`);
+    // eslint-disable-next-line no-await-in-loop
+    await DB.collection(COLLECTION.USER).insertOne({
+      ...user,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    });
+
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.log(e);
+  }
+  next();
 }
 
-module.exports.down = function (next) {
-  next()
-}
+module.exports.down = function down(next) {
+  next();
+};
